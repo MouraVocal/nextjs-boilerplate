e7f0e63703d838806a637741a87572689b1ca3bb
mouravocal <diego.moura3@gmail.com>
2024-09-01 10:43:38 -0300

    chore: :sparkles: adds jest to lintstagedrc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,9 @@ const path = require('path')
 const buildEslintCommand = filenames =>
   `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
+const buildJestCommand = filenames =>
+  `npm run test -- --onlyChanged ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand]
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildJestCommand]
 }
