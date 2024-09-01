592938b7e7f076523806d7572bf4dd06581a61c3
mouravocal <diego.moura3@gmail.com>
2024-09-01 10:53:56 -0300

    chore: :sparkles: changes jest flag and calls npm test scritp

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,7 @@ const buildEslintCommand = filenames =>
   `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
 const buildJestCommand = filenames =>
-  `npm run test -- --onlyChanged ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
+  `npm test -- --findRelatedTests ${filenames.map(f => path.relative(process.cwd(), f)).join(' --file ')}`
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildJestCommand]
